49dee727fff114e51326a9bf8e7c75051348d6c1
kubrabayazit <kubrabayazitt@gmail.com>
2021-01-21 15:14:48 +0300

    no message

---
+++ GlobalvsLocal/app.js
@@ -0,0 +1,37 @@
+let PlanComponent = {
+    template: '#plan-template',
+    props: {
+      name: {
+        type: String,
+        required: true
+      }
+    }
+  }
+  
+
+let PlanPickerComponent = {
+    template: '#plan-picker-template',
+    components: {
+        plan: PlanComponent
+    },
+    data () {
+      return {
+        plans: ['The Single', 'The Curious', 'The Addict']
+      }
+    }
+  }
+  
+  
+    new Vue({
+      el: '#app',
+      components: {
+          'plan-picker': PlanPickerComponent
+      }
+     
+    }) 
+
+   /* new Vue({
+      el: '#app2'
+      
+     
+    }) */
